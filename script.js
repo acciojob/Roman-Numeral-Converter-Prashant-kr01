128552f0d1908fb8de368355dbd35e3876ace037
Prashant kumar <114512068+Prashant-kr01@users.noreply.github.com>
2025-04-04 15:26:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,48 +1,17 @@
-function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
-
-  //your code here
-
+function reset() {
+    for (i = 1; i <= 9; i++) {
+        const block = document.getElementById(`${i}`);
+        block.style.backgroundColor = "transparent";
+    }
 }
-    let result = "";
   
-    // Handling special subtractive cases separately
-    const subtractiveCases = {
-        900: "CM",
-        400: "CD",
-        90: "XC",
-        40: "XL",
-        9: "IX",
-        4: "IV"
-    };
+document.getElementById('reset_button').addEventListener('click', reset);
 
-	for(let key in subtractiveCases){
-		if(num >= key){
-			result+= subtractiveCases[key];
-			num -=key;
-		}
-	}
-  for(let i in obj){
-	  if(num >= obj[i][0]){
-		 result += obj[i][0];
-		  num -= obj[i][1];
-	  }
-  }
-return result;
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
-
-// console.log(convertToRoman(36));
-
-
-
-
-// do not edit below this line
-module.exports = convertToRoman
+document.getElementById('change_button').addEventListener('click', () => {
+    reset();
+    const blockId = document.getElementById("block_id").value;
+    const color = document.getElementById("colour_id").value;
+    // alert(colorId)
+    const block = document.getElementById(`${blockId}`);
+    block.style.backgroundColor = color;
+});
